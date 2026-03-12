f313e407a212836da47de8e790ab17c8e008acda
Ices668 <InadaOsvaldo79@gmail.com>
2026-03-12 10:16:01 +0800

    chore: remove gkx-prototype-react

--- gkx-prototype-react/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Serve prototype assets from /gkx/ so we can reuse the original URLs as-is.
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
